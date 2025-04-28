ca665b89389078a3319b22efa3b83bcc4826d80d
Matteocent <194429410+Matteocent@users.noreply.github.com>
2025-04-28 09:43:11 +0100

    Add index page and script with functions. The functions allow to get an input from players and play a single round. Not completed yet.

---
+++ myscript.js
@@ -0,0 +1,57 @@
+/*Brief presentation about the game*/
+console.log("Game of 'Rock' 'Paper' 'Scissors'");
+function getHumanChoice() {
+    let HumanChoice = prompt("Enter your choice between 'Rock' 'Paper' 'Scissors':");
+    return HumanChoice;
+}
+
+function getComputerChoice() {
+    let ComputerChoice = Math.floor(Math.random() * 10);
+    return ComputerChoice;
+} 
+
+let HumanScore = 0;
+let ComputerScore = 0;
+
+/*In this function the argument names are casual, not referred to previous functions */
+function playRound(HumanChoice, ComputerChoice) {
+    let output;
+    if (HumanChoice == "Rock") {
+        if (ComputerChoice <= 3) {
+            output = ("Try again, you both choose 'Rock'.");
+        } else if (ComputerChoice > 3 && ComputerChoice <= 6) {
+            output = ("Paper beats Rock, Computer wins.");
+            ComputerScore ++;
+        } else if (ComputerChoice > 6) {
+            output = ("You won, Rock beats scissors");
+            HumanScore ++;
+        }
+    }
+    if (HumanChoice == "Paper") {
+        if (ComputerChoice <= 3) {
+            output = ("You won, Paper beats Rock.");
+            HumanScore ++;
+        } else if (ComputerChoice > 3 && ComputerChoice <= 6) {
+            output = ("Try again, you both choose Paper");
+        } else if (ComputerChoice > 6) {
+            output = ("Scissors beats Paper, Computer wins");
+            ComputerScore ++;
+        }
+    }
+    if (HumanChoice == "Scissors") {
+        if (ComputerChoice <= 3) {
+            output = ("Rock beats Scissors, Computer wins.")
+            ComputerScore ++;
+        } else if (ComputerChoice > 3 && ComputerChoice <= 6) {
+            output = ("You won, Scissors beats Paper");
+            HumanScore ++;
+        } else if (ComputerChoice > 6) {
+            output = ("Try again, you both choose Scissors");
+        }
+    }
+    console.log(HumanChoice);
+    console.log(ComputerChoice);
+    return output;
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
